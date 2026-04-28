9d8177a16887bf5e465a9ca059dcd534d776fedf
RyanLin-InfEvo <ryanarduino0410@gmail.com>
2026-04-28 22:13:46 +0800

    refactor(work): adopt consistent model pattern with std::optional and json macros

--- core/src/models/work.h
+++ core/src/models/work.h
@@ -5,18 +5,41 @@
  */
 
 #pragma once
+
+#include <nlohmann/json.hpp>
+#include <optional>
 #include <string>
+
+#include "../utils/optional_helper.h"
 
 namespace lyra {
 
 struct Work {
     std::string id;
     std::string title;
-    int composition_start_year = 0;
-    int composition_end_year = 0;
-    std::string composition_date_text;
-    std::string iswc;
-    std::string musicbrainz_id;
+    std::optional<uint16_t> composition_start_year;
+    std::optional<uint16_t> composition_end_year;
+    std::optional<std::string> composition_date_text;
+    std::optional<std::string> iswc;
+    std::optional<std::string> musicbrainz_id;
 };
+
+struct WorkUpdate {
+    std::string id;
+    std::optional<std::string> title;
+    std::optional<uint16_t> composition_start_year;
+    std::optional<uint16_t> composition_end_year;
+    std::optional<std::string> composition_date_text;
+    std::optional<std::string> iswc;
+    std::optional<std::string> musicbrainz_id;
+
+    [[nodiscard]] bool has_updates() const noexcept {
+        return utils::any_has_value(title, composition_start_year, composition_end_year,
+                                    composition_date_text, iswc, musicbrainz_id);
+    }
+};
+
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_WITH_DEFAULT(Work, id, title, composition_start_year, composition_end_year, composition_date_text, iswc, musicbrainz_id)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_WITH_DEFAULT(WorkUpdate, id, title, composition_start_year, composition_end_year, composition_date_text, iswc, musicbrainz_id)
 
 } // namespace lyra
